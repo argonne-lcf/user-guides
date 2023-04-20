9657b1d66702752ca988a0aaa3e080a32673786d
Varuni Sastry <88804132+vksastry@users.noreply.github.com>
2023-04-20 12:41:15 -0500

    Update unet_batch.sh

--- docs/ai-testbed/sambanova_gen2/files/unet_batch.sh
+++ docs/ai-testbed/sambanova_gen2/files/unet_batch.sh
@@ -1,13 +1,26 @@
-unet_batch.sh
-#! /bin/bash -x
-#set -e
+#! /bin/bash
+set -e
+LOGDIR=`date +%m%d%y.%H`
+if [ "$5" ] ; then
+LOGDIR=$5
+fi
+MODEL_NAME="Unet2d"
+OUTPUT_PATH=/data/ANL/results/$(hostname)/${USER}/${LOGDIR}/${MODEL_NAME}.out
+echo "Using ${OUTPUT_PATH} for output"
+mkdir -p /data/ANL/results/$(hostname)/${USER}/${LOGDIR}
+
+#######################
+export OMP_NUM_THREADS=8
+#######################
+# Start script timer
 SECONDS=0
-BS=32
-IM=32
+# Temp file location
+OUTDIR=${HOME}/out/${MODEL_NAME}
+
 BS=$3
 IM=$4
 DS=/data/ANL/kaggle_3m
-CACHE_DIR=/nvmedata/scratch/${USER}/kaggle_3m_${IM}
+CACHE_DIR=/data/scratch/${USER}/kaggle_3m_${IM}
 if [ ! -d ${CACHE_DIR} ] ; then
   mkdir -p ${CACHE_DIR}
 fi
@@ -30,9 +43,9 @@ else
   exit
 fi
 
-echo "Model: UNET_TRAIN"
-echo "Date: " $(date +%m/%d/%y)
-echo "Time: " $(date +%H:%M)
+echo "Model: ${MODEL_NAME} " > ${OUTPUT_PATH} 2>&1
+echo "Date: " $(date +%m/%d/%y) >> ${OUTPUT_PATH} 2>&1
+echo "Time: " $(date +%H:%M) >> ${OUTPUT_PATH} 2>&1
 
 #    export SAMBA_CCL_USE_PCIE_TRANSPORT=0
     export SF_RNT_NUMA_BIND=2
@@ -48,8 +61,11 @@ echo "Time: " $(date +%H:%M)
     rm -rf log_dir_unet_${NP}_train_kaggle
     if [ -e ${UNET}/hook.py ] ; then
     #orig   srun --mpi=pmi2 python ${UNET}/hook.py  run --data-cache-dir ${CACHE_DIR}  --num-workers=${NUM_WORKERS} --mode train --in-channels=3 --in-width=${IM} --in-height=${IM} --init-features 32 --batch-size=${BS} --epochs 10  --data-dir ${DS} --log-dir log_dir_unet_${IM}_${BS}_${NP} --pef=$(pwd)/out/unet_train_${BS}_${IM}_NP/unet_train_${BS}_${IM}_NP.pef --data-parallel --reduce-on-rdu --use-sambaloader > run_unet_${BS}_${IM}_${NP}.log 2>&1
-    srun --mpi=pmi2 python ${UNET}/hook.py  run --data-in-memory --data-cache=${CACHE_DIR}  --num-workers=${NUM_WORKERS} --in-channels=3 --in-width=${IM} --in-height=${IM} --init-features 32 --batch-size=${BS} --epochs 10  --data-dir ${DS} --log-dir log_dir_unet_${IM}_${BS}_${NP} --data-parallel --reduce-on-rdu --pef=$(pwd)/out/unet_train_${BS}_${IM}_NP_4/unet_train_${BS}_${IM}_NP_4.pef > run_unet_${BS}_${IM}_${NP}_4.log 2>&1
+    #1.15.2 srun --mpi=pmi2 python ${UNET}/hook.py  run --data-in-memory --data-cache=${CACHE_DIR}  --num-workers=${NUM_WORKERS} --in-channels=3 --in-width=${IM} --in-height=${IM} --init-features 32 --batch-size=${BS} --epochs 10  --data-dir ${DS} --log-dir log_dir_unet_${IM}_${BS}_${NP} --data-parallel --reduce-on-rdu --pef=$(pwd)/out/unet_train_${BS}_${IM}_NP_4/unet_train_${BS}_${IM}_NP_4.pef > run_unet_${BS}_${IM}_${NP}_4.log 2>&1
+  COMMAND="srun --mpi=pmi2 python /opt/sambaflow/apps/image/segmentation//hook.py run --data-cache=${CACHE_DIR}  --data-in-memory --num-workers=${NUM_WORKERS} --enable-tiling  --min-throughput 395 --in-channels=3 --in-width=${IM} --in-height=${IM} --init-features 32 --batch-size=${BS} --epochs 10 --data-dir ${DS} --log-dir log_dir_unet_${IM}_${BS}_${NP} --data-parallel --reduce-on-rdu --pef=${OUTDIR}/unet_train_${BS}_${IM}_NP_4/unet_train_${BS}_${IM}_NP_4.pef"
     else
-        srun --mpi=pmi2 python ${UNET}/unet_hook.py  run --data-cache-dir ${CACHE_DIR} --num-workers=${NUM_WORKERS} --do-train --in-channels=3 --in-width=${IM} --in-height=${IM} --init-features 32 --batch-size=${BS} --epochs 10  --data-dir ${DS} --log-dir log_dir_unet_${IM}_${BS}_${NP} --pef=$(pwd)/out/unet_train_${BS}_${IM}_NP/unet_train_${BS}_${IM}_NP.pef --data-parallel --reduce-on-rdu --use-sambaloader > run_unet_${BS}_${IM}_${NP}.log 2>&1
+        COMMAND="srun --mpi=pmi2 python ${UNET}/unet_hook.py  run --data-cache-dir ${CACHE_DIR} --num-workers=${NUM_WORKERS} --do-train --in-channels=3 --in-width=${IM} --in-height=${IM} --init-features 32 --batch-size=${BS} --epochs 10  --data-dir ${DS} --log-dir log_dir_unet_${IM}_${BS}_${NP} --pef=$(pwd)/out/unet_train_${BS}_${IM}_NP/unet_train_${BS}_${IM}_NP.pef --data-parallel --reduce-on-rdu --use-sambaloader > run_unet_${BS}_${IM}_${NP}.log 2>&1"
     fi
-echo "Duration: " $SECONDS
+echo $COMMAND >> ${OUTPUT_PATH} 2>&1
+eval $COMMAND >> ${OUTPUT_PATH} 2>&1
+echo "Duration: " $SECONDS >> ${OUTPUT_PATH} 2>&1                                                              
